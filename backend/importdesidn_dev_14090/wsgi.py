0c8edbbe04a7934cfa6275eebea054ee1e9b58cb
Crowdbotics <team@crowdbotics.com>
2020-10-27 10:13:07 +0000

    Update models

--- backend/importdesidn_dev_14090/wsgi.py
+++ backend/importdesidn_dev_14090/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'importdesidn_dev_14090.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "importdesidn_dev_14090.settings")
 
 application = get_wsgi_application()
